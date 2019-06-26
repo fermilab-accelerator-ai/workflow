155b9255e124f407f8f3b3e967d7d656d5e6ab83
Jason St. John <jason.st.john@gmail.com>
2019-06-26 14:49:17 -0500

    First draft. Shares commandline options with DataGrabber.

---
+++ EventDataGrabber.py
@@ -0,0 +1,129 @@
+import h5py
+from io import StringIO
+import matplotlib
+matplotlib.use('agg')  #Workaround for missing tkinter
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import urllib
+import time
+import datetime
+from dateutil import parser as dtpars
+import timeit
+import requests
+import argparse
+from pathlib import Path
+from bs4 import BeautifulSoup
+
+# optparse is outstanding, handling many types and
+# generating a --help very easily.  Typical Python module with named, optional arguments
+# For instance:
+### Make a parser
+parser = argparse.ArgumentParser(description="usage: %prog [options] <input file.ROOT> \n")
+### Add options
+parser.add_argument ('-v', dest='debug', action="store_true", default=False,
+                   help="Turn on verbose debugging.")
+# Maybe just a string formatted in UTC datetime.
+parser.add_argument ('--stopat',  dest='stopat', default='',
+                   help="YYYY-MM-DD hh:mm:ss")
+parser.add_argument ('--days', dest='days', type=float, default=0,
+                   help="Days before start time to request data? Default zero.")
+parser.add_argument ('--hours', dest='hours', type=float, default=0,
+                   help="Hours before start time to request data? Default zero.")
+parser.add_argument ('--minutes', dest='minutes', type=float, default=0,
+                   help="Minutes before start time to request data? Default zero.")
+parser.add_argument ('--seconds', dest='seconds', type=float, default=0,
+                   help="Seconds before start time to request data? Default zero.")
+parser.add_argument ('--outdir',  dest='outdir', default='',
+                   help="Directory to write output file.")
+### Get the options and argument values from the parser....
+options = parser.parse_args()
+### ...and assign them to variables. (No declaration needed, just like bash!)
+debug   = options.debug
+stopat  = options.stopat
+days    = options.days    
+hours   = options.hours   
+minutes = options.minutes 
+seconds = options.seconds 
+outdir  = options.outdir
+
+unixtimestr = str(time.time())
+# Datetime for when to stop the reading
+today = datetime.datetime.today()
+
+stopptime = ''
+if stopat == '': #Default: Midnight at the start of today
+    stopptime = '{0:%Y-%m-%d+00:00:00}'.format(today )
+else:            # or attempt to use the string passed in by user
+    stopdt =  dtpars.parse(stopat)
+    stopptime = '{0:%Y-%m-%d+%H:%M:%S}'.format(stopdt)
+
+if debug: print ("Stop time: "+stopptime)
+    
+# If no time interval set, default to 1 second
+if days == 0 and hours == 0 and minutes == 0 and seconds == 0: seconds = 1
+
+if debug:
+    print ("Time interval: days = "+str(days)+
+           ", hours = "  +str(hours  )+
+           ", minutes = "+str(minutes)+
+           ", seconds = "+str(seconds)+".")
+
+def read_URL_to_file (URL, filename):
+    with urllib.request.urlopen(URL) as url_returns:
+        data = url_returns.read().decode('utf-8').split('\n')
+        with open(filename, 'w') as f:
+            for datum in data:
+                f.write("%s\n" % datum)
+                
+    return 
+# Build a datetime interval to offset starttime before stopptime. 
+interval = datetime.timedelta(days=days, hours=hours, minutes=minutes, seconds=seconds)
+
+# Set the time to start the data-series request window
+starttime = '{0:%Y-%m-%d+%H:%M:%S}'.format(dtpars.parse(stopptime.replace('+',' ')) - interval)
+
+if debug:
+    print ('Data request start time:' + starttime)
+    print ('Data request stop time:' + stopptime)
+
+D43DataLoggerNode = 'EventC'
+#Fun with hdf5
+abspath = Path().absolute()
+current_dir = abspath
+if outdir == '':
+    outdir = str(current_dir) + '/'
+outfilename = outdir+'/MLEventData_'+unixtimestr+'_From_'+D43DataLoggerNode+'_'+starttime+'_to_'+stopptime+'.h5'
+
+    
+# Retrieve the timestamps of broadcasting for every TCLK event over this time span.
+URL = "http://www-ad.fnal.gov/cgi-bin/acl.pl?acl=event_log/start_time=\""+starttime+"\"/stop_time=\""+stopptime+"\"/event="
+for eventdec in range(0, 0xFF+1):
+    TCLKevent ='{:02x}'.format(eventdec)
+    thisURL = URL + TCLKevent
+    if debug: print (thisURL)
+    response = requests.get(thisURL)
+    ## Did we get anything?
+    if response is None:
+        if debug: print (thisURL+"\n begat no reponse.")
+        continue
+    soup = BeautifulSoup(response.text, "html.parser")
+    str1 = soup.get_text()
+    if debug: print (str1)
+    if str1.count('No occurrences of event') > 0: continue
+    ## Easy to make a dataframe from the results. And add them to an appropriately keyed group in the hdf5?
+    df = pd.read_csv(StringIO(str1), header=None, delim_whitespace=True)
+    # Set the column names
+    df.columns = ['date','time','SCmicrosec']
+    # Merge date and time to make datetime. Then drop original columns.
+    df['datetime'] = df['date'] + '+' + df['time'] 
+    dropthese = ['date','time']
+    df.drop(dropthese, inplace=True, axis=1)
+    if debug: print (df)
+    # Save df to file.
+    df.to_hdf(outfilename,'Event'+TCLKevent, append=True)
+    
+
+
+
+
